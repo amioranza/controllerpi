45be906712ca575ec72dbc02046a9f20e26c9b81
A. Mioranza <amioranza@mdcnet.ninja>
2019-01-20 21:42:33 -0200

    Minor improvments

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"encoding/json"
 	"flag"
 	"fmt"
 	"log"
@@ -28,29 +27,33 @@ type App struct {
 func int32Ptr(i int32) *int32 { return &i }
 
 func DeployApp(w http.ResponseWriter, r *http.Request) {
+	log.Printf("Raw params: %v", mux.Vars(r))
 	params := mux.Vars(r)
-	var app App
-	_ = json.NewDecoder(r.Body).Decode(&app)
-	app.Node = params["node"]
-	app.Pin = params["pin"]
-	app.Status = params["status"]
-	log.Printf("Params: %s\n", app.Status)
+	// node := params["node"]
+	// pin := params["pin"]
+	status := params["status"]
+	log.Printf("Params: %s\n", params)
 
-	//config kubernetes access, intra cluster
+	// Bootstrap k8s configuration from local 	Kubernetes config file
+	kubeconfig := filepath.Join(os.Getenv("HOME"), ".kube", "config")
+	log.Println("Using kubeconfig file: ", kubeconfig)
+	// config kubernetes access, ext cluster, uncomment below
+	//config kubernetes access, intra cluster, uncomment below
+	// config, err := clientcmd.BuildConfigFromFlags("", kubeconfig)
 	config, err := clientcmd.BuildConfigFromFlags("", "")
 	if err != nil {
 		log.Fatal(err)
 	}
 	clientset, err := kubernetes.NewForConfig(config)
-	deploymentsClient := clientset.AppsV1().Deployments(apiv1.NamespaceDefault)
+	deploymentsClient := clientset.AppsV1().Deployments("pi-system")
 
-	if app.Status == "true" {
+	if status == "true" {
 		deployment := &appsv1.Deployment{
 			ObjectMeta: metav1.ObjectMeta{
 				Name: "led-deployment",
 			},
 			Spec: appsv1.DeploymentSpec{
-				Replicas: int32Ptr(2),
+				Replicas: int32Ptr(1),
 				Selector: &metav1.LabelSelector{
 					MatchLabels: map[string]string{
 						"app": "led",
@@ -97,6 +100,9 @@ func DeployApp(w http.ResponseWriter, r *http.Request) {
 		if err := deploymentsClient.Delete("led-deployment", &metav1.DeleteOptions{
 			PropagationPolicy: &deletePolicy,
 		}); err != nil {
+			// log.Println("ERROU!!!")
+			w.WriteHeader(http.StatusOK)
+			fmt.Fprintf(w, "<html><h1>ERROU!!!</h1></html>")
 			panic(err)
 		}
 		fmt.Println("Deleted deployment.")
@@ -114,52 +120,54 @@ func SayHello(w http.ResponseWriter, r *http.Request) {
 // kubectl get pods
 //
 func main() {
-	go func() { // new restfull api server
-		router := mux.NewRouter()
-		router.HandleFunc("/", DeployApp).Methods("POST")
-		router.HandleFunc("/", SayHello).Methods("GET")
-		log.Fatal(http.ListenAndServe(":8000", router))
+
+	go func() {
+		var ns string
+		flag.StringVar(&ns, "namespace", "", "namespace")
+
+		// Bootstrap k8s configuration from local 	Kubernetes config file
+		kubeconfig := filepath.Join(os.Getenv("HOME"), ".kube", "config")
+		log.Println("Using kubeconfig file: ", kubeconfig)
+		//config, err := clientcmd.BuildConfigFromFlags("", kubeconfig)
+		config, err := clientcmd.BuildConfigFromFlags("", "")
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		// Create an rest client not targeting specific API version
+		clientset, err := kubernetes.NewForConfig(config)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		for {
+			pods, err := clientset.CoreV1().Pods("pi-system").List(metav1.ListOptions{})
+			if err != nil {
+				log.Fatalln("failed to get pods:", err)
+			}
+
+			nodes, err := clientset.CoreV1().Nodes().List(metav1.ListOptions{})
+			if err != nil {
+				log.Fatalln("failed to get nodes:", err)
+			}
+
+			// print pods
+			fmt.Println("\n______________________   P O D S    _________________________")
+			for i, pod := range pods.Items {
+				fmt.Printf("[%d] %s\n", i, pod.GetName())
+			}
+			fmt.Println("\n______________________   N O D E S    ________________________")
+			for j, node := range nodes.Items {
+				fmt.Printf("[%d] %s - %s\n", j, node.GetName(), node.GetLabels())
+			}
+			log.Println("Sleeping 5s")
+			time.Sleep(5000 * time.Millisecond)
+		}
 	}()
+	// new restfull api server
+	router := mux.NewRouter()
+	router.HandleFunc("/{pin}/{status}", DeployApp).Methods("POST")
+	router.HandleFunc("/", SayHello).Methods("GET")
+	log.Fatal(http.ListenAndServe(":8000", router))
 
-	var ns string
-	flag.StringVar(&ns, "namespace", "", "namespace")
-
-	// Bootstrap k8s configuration from local 	Kubernetes config file
-	kubeconfig := filepath.Join(os.Getenv("HOME"), ".kube", "config")
-	log.Println("Using kubeconfig file: ", kubeconfig)
-	config, err := clientcmd.BuildConfigFromFlags("", kubeconfig)
-	// config, err := clientcmd.BuildConfigFromFlags("", "")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// Create an rest client not targeting specific API version
-	clientset, err := kubernetes.NewForConfig(config)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	for {
-		pods, err := clientset.CoreV1().Pods("").List(metav1.ListOptions{})
-		if err != nil {
-			log.Fatalln("failed to get pods:", err)
-		}
-
-		nodes, err := clientset.CoreV1().Nodes().List(metav1.ListOptions{})
-		if err != nil {
-			log.Fatalln("failed to get nodes:", err)
-		}
-
-		// print pods
-		fmt.Println("+#+#+#+#   PODS    +#+#+#+#")
-		for i, pod := range pods.Items {
-			fmt.Printf("[%d] %s\n", i, pod.GetName())
-		}
-		fmt.Println("+#+#+#+#   NODES   +#+#+#+#")
-		for j, node := range nodes.Items {
-			fmt.Printf("[%d] %s - %s\n", j, node.GetName(), node.GetLabels())
-		}
-		log.Println("Sleeping 5s")
-		time.Sleep(5000 * time.Millisecond)
-	}
 }
